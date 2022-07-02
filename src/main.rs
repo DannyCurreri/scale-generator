c82bdddf728ec9adf2bea84afb2bb4011d787075
Daniel Curreri <danielcurreri@gmail.com>
2022-07-02 17:48:20 -0400

    Print scale notes in cleaner format
    Modify main() to print a string with all scale notes separated by spaces
    rather than in the debug vec format.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use std::env;
 
 fn main() {
     match Scale::from_args(env::args()) {
-        Ok(s) => println!("{} scale: {:?}", s.name(), s.enumerate()),
+        Ok(s) => {
+            let notes: String = s.enumerate().iter().map(|note| format!("{} ", note)).collect();
+            println!("{} scale: {}", s.name(), notes);
+        },
         Err(e) => eprintln!("Error: {}", e),
     }
 }
