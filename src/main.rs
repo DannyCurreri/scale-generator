2abce9291a7d0a6f10ff10b7d642cbc1dba9f3f4
Daniel Curreri <danielcurreri@gmail.com>
2022-08-03 12:59:40 -0400

    apply cargo format to main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use std::env;
 fn main() {
     match Scale::from_args(env::args()) {
         Ok(s) => {
-            let notes: String = s.enumerate().iter().map(|note| format!("{} ", note)).collect();
+            let notes: String = s
+                .enumerate()
+                .iter()
+                .map(|note| format!("{} ", note))
+                .collect();
             println!("{} scale: {}", s.name(), notes);
-        },
+        }
         Err(e) => eprintln!("Error: {}", e),
     }
 }
